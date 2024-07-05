3c77f49337fdf7d689078d678daed3431b5a2328
Ishanka94 <tharindu.ishanka1994@gmail.com>
2024-07-05 17:49:20 +0400

    Status dropdown changes

--- src/pages/doctor-management/DoctorsDetails.js
+++ src/pages/doctor-management/DoctorsDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, Fragment  } from 'react';
 import AddDoctorDetails from './AddDoctorDetails';
 import EditDoctorDetails from './EditDoctorDetails';
 import AuthContext from '../../contexts/AuthContext';
@@ -11,6 +11,18 @@ import { PencilIcon, Bars3Icon } from '@heroicons/react/20/solid';
 import DisplayDoctorInfo from './DisplayDoctorInfo';
 import {useDispatch, useSelector} from "react-redux";
 import { updateDoctors, showDocModel, setCurrentDoctor, showDocEditModel, showDocInfoModel } from "../../actions/doctorActions";
+import { Dialog, Transition, Listbox } from "@headlessui/react";
+import * as DoctorStatus from '../../util/constants/DoctorStatus';
+import { CheckIcon, ChevronUpDownIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
+
+const ALL = 'ALL';
+
+const docStatus = [
+  { name: 'All', key: ALL  },
+  { name: 'Active', key: DoctorStatus.ACTIVE  },
+  { name: 'Inactive', key: DoctorStatus.INACTIVE },
+  { name: 'Pending', key: DoctorStatus.PENDING_APPROVAL }
+]
 
 function DoctorsDetails() {
 
@@ -20,7 +32,7 @@ function DoctorsDetails() {
   const showDoctorInfoModal = useSelector(state => state.doctor.showDoctorInfoModal)
   const doctors = useSelector(state => state.doctor.doctors)
   const doctor = useSelector(state => state.doctor.doctor)
-
+  const [selected, setSelected] = useState(docStatus[0]);
   const [updatePage, setUpdatePage] = useState(true);
   const [pageCount, setpageCount] = useState(0);
 
@@ -103,6 +115,62 @@ function DoctorsDetails() {
             <div className="flex gap-4 justify-center items-center ">
               <span className="font-bold">Doctor Details</span>
             </div>
+            
+           
+            <label>Status</label>
+            <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-1">
+                  <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate">{selected.name}</span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <ChevronDownIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                      {docStatus.map((status, statusIdx) => (
+                        <Listbox.Option
+                          key={statusIdx}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                            }`
+                          }
+                          value={status}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? 'font-medium' : 'font-normal'
+                                }`}
+                              >
+                                {status.name}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+
+
+            
             <div className="flex gap-4">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 text-xs  rounded"
